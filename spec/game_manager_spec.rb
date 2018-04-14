e3f00ed19950004d1917b8b3c1ae13bb0125db16
IvyMic <ibrahim_e@hotmail.co.uk>
2018-04-14 03:48:37 +0100

    attempting human vs human integration test

--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -7,12 +7,25 @@ describe GameManager do
 
   describe "#setup_game" do
 
-    context "in an ai vs ai case" do
+    # context "in an ai vs ai case" do
+    #   before do
+    #     allow_any_instance_of(IO).to receive(:puts)
+    #     $stdout.stub(:write)
+    #     allow(game_manager).to receive(:get_mode).and_return(3)
+    #     allow(game_manager).to receive(:show_player_order).and_return(1)
+    #   end
+    #
+    #   it "plays a game until game is over" do
+    #     game_manager.setup_game
+    #     expect(game_manager.board.game_over?).to be(true)
+    #   end
+    # end
+
+    context "in a human vs human case" do
       before do
-        allow_any_instance_of(IO).to receive(:puts)
         $stdout.stub(:write)
-        allow(game_manager).to receive(:get_mode).and_return(3)
-        allow(game_manager).to receive(:show_player_order).and_return(1)
+        allow(game_manager).to receive(:gets).and_return("1","1","Goku","Vegeta","1","1","4","2","5","3")
+        #game_manager.stub(:gets).and_return("1","1","Goku","Vegeta","1","1","4","2","5","3")
       end
 
       it "plays a game until game is over" do
